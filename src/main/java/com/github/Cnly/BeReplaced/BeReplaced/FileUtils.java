dcdf087da70cc6a7b554d3d5a22227ad93489543
Cnly <minecnly@gmail.com>
2014-09-09 15:46:20 +0800

    Formatted the code.

--- src/main/java/com/github/Cnly/BeReplaced/BeReplaced/FileUtils.java
+++ src/main/java/com/github/Cnly/BeReplaced/BeReplaced/FileUtils.java
@@ -10,56 +10,63 @@ import java.io.OutputStream;
 
 public class FileUtils
 {
-	
-	public static void copyFile(File source, File dest)
-	{
-		
-		InputStream input = null;
-		OutputStream output = null;
-		
-		try {
-			
-			input = new FileInputStream(source);
-			output = new FileOutputStream(dest);
-			byte[] buf = new byte[1024];
-			int bytesRead;
-			while ((bytesRead = input.read(buf)) != -1) output.write(buf, 0, bytesRead);
-			
-		}
-		catch (FileNotFoundException e)
-		{
-			Main.getInstance().getLogger().severe("File not found!");
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			Main.getInstance().getLogger().severe("An error occurred while copying the file!");
-			e.printStackTrace();
-		}
-		finally
-		{
-			
-			try
-			{
-				input.close();
-			}
-			catch (IOException e)
-			{
-				Main.getInstance().getLogger().severe("An error occurred while closing the input stream!");
-				e.printStackTrace();
-			}
-			
-			try
-			{
-				output.close();
-			}
-			catch (IOException e)
-			{
-				Main.getInstance().getLogger().severe("An error occurred while closing the output stream!");
-				e.printStackTrace();
-			}
-			
-		}
-	}
-	
+    
+    public static void copyFile(File source, File dest)
+    {
+        
+        InputStream input = null;
+        OutputStream output = null;
+        
+        try
+        {
+            
+            input = new FileInputStream(source);
+            output = new FileOutputStream(dest);
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buf)) != -1)
+                output.write(buf, 0, bytesRead);
+            
+        }
+        catch (FileNotFoundException e)
+        {
+            Main.getInstance().getLogger().severe("File not found!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            Main.getInstance().getLogger()
+                    .severe("An error occurred while copying the file!");
+            e.printStackTrace();
+        }
+        finally
+        {
+            
+            try
+            {
+                input.close();
+            }
+            catch (IOException e)
+            {
+                Main.getInstance()
+                        .getLogger()
+                        .severe("An error occurred while closing the input stream!");
+                e.printStackTrace();
+            }
+            
+            try
+            {
+                output.close();
+            }
+            catch (IOException e)
+            {
+                Main.getInstance()
+                        .getLogger()
+                        .severe("An error occurred while closing the output stream!");
+                e.printStackTrace();
+            }
+            
+        }
+    }
+    
 }
--- src/main/java/com/github/Cnly/BeReplaced/BeReplaced/Main.java
+++ src/main/java/com/github/Cnly/BeReplaced/BeReplaced/Main.java
@@ -11,82 +11,85 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
 {
-	
-	private static Main instance = null;
-	
-	@Override
-	public void onEnable()
-	{
-		
-		instance = this;
-		
-		saveDefaultConfig();
-		
-		getCommand("ber").setExecutor(this);
-		
-	}
-	
-	@Override
-	public void onDisable()
-	{
-		// Do nothing.
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-	{
-		
-		List<String> pathList = getConfig().getStringList("pluginPaths");
-		
-		if(!sender.hasPermission("BeReplaced.use"))
-		{
-			
-			sender.sendMessage(ChatColor.RED + "You don't have permission!");
-			
-			return true;
-		}
-		
-		if(pathList.isEmpty())
-		{
-			
-			getLogger().info("No plugins to be replaced.");
-			
-			return true;
-		}
-		
-		getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " By Cnly");
-		getLogger().info("Replacing started.");
-		
-		for(String path : pathList)
-		{
-			
-			File f = new File(path);
-			File d = new File("plugins/" + f.getName());
-			
-			if(!f.exists())
-			{
-				
-				getLogger().info("File " + path + "not exists. Skipped.");
-				
-				continue;
-			}
-			
-			FileUtils.copyFile(f, d);
-			
-			getLogger().info("Replaced: " + f.getName());
-			
-		}
-		
-		getLogger().info("All files replaced. Reloading server...");
-		
-		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "reload");
-		
-		return true;
-	}
-
-	public static Main getInstance()
-	{
-		return instance;
-	}
-	
+    
+    private static Main instance = null;
+    
+    @Override
+    public void onEnable()
+    {
+        
+        instance = this;
+        
+        saveDefaultConfig();
+        
+        getCommand("ber").setExecutor(this);
+        
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        // Do nothing.
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command,
+            String label, String[] args)
+    {
+        
+        List<String> pathList = getConfig().getStringList("pluginPaths");
+        
+        if (!sender.hasPermission("BeReplaced.use"))
+        {
+            
+            sender.sendMessage(ChatColor.RED + "You don't have permission!");
+            
+            return true;
+        }
+        
+        if (pathList.isEmpty())
+        {
+            
+            getLogger().info("No plugins to be replaced.");
+            
+            return true;
+        }
+        
+        getLogger().info(
+                getDescription().getName() + " "
+                        + getDescription().getVersion() + " By Cnly");
+        getLogger().info("Replacing started.");
+        
+        for (String path : pathList)
+        {
+            
+            File f = new File(path);
+            File d = new File("plugins/" + f.getName());
+            
+            if (!f.exists())
+            {
+                
+                getLogger().info("File " + path + "not exists. Skipped.");
+                
+                continue;
+            }
+            
+            FileUtils.copyFile(f, d);
+            
+            getLogger().info("Replaced: " + f.getName());
+            
+        }
+        
+        getLogger().info("All files replaced. Reloading server...");
+        
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "reload");
+        
+        return true;
+    }
+    
+    public static Main getInstance()
+    {
+        return instance;
+    }
+    
 }
